bca96f989b3cc76dc3147b83f64323f0e4068f09
Waywaydev <wayway.dev@gmail.com>
2022-10-10 18:01:53 +0900

    git Ignore Update

--- Editor/GameEventDrawer.cs
+++ Editor/GameEventDrawer.cs
@@ -23,18 +23,6 @@ namespace Wayway.Engine.Events.Editor
             }
         }
     }
-
-    public class GameEventListenerDrawer : OdinAttributeProcessor<GameEventListenerCore>
-    {
-        public override void ProcessChildMemberAttributes(InspectorProperty parentProperty, MemberInfo member, List<Attribute> attributes)
-        {
-            if (member.Name == "priority")
-            {
-                attributes.Add(new PropertyRangeAttribute(0, 10));
-                attributes.Add(new HideLabelAttribute());
-            }
-        }
-    }
     
     public class GameEventDrawer<T, F> : OdinAttributeProcessor<T> where T : GameEvent<F>
     {
@@ -67,6 +55,18 @@ namespace Wayway.Engine.Events.Editor
             }
         }
     }
+
+    public class GameEventListenerDrawer : OdinAttributeProcessor<GameEventListenerCore>
+    {
+        public override void ProcessChildMemberAttributes(InspectorProperty parentProperty, MemberInfo member, List<Attribute> attributes)
+        {
+            if (member.Name == "priority")
+            {
+                attributes.Add(new PropertyRangeAttribute(0, 10));
+                attributes.Add(new HideLabelAttribute());
+            }
+        }
+    }   
 }
 #endif
 
